e652b39a004556670e9b1121a7ec615ae0de1d81
xs3508198 <1131259489@qq.com>
2025-03-20 16:48:38 +0800

    test

--- src/main/java/run/mone/github/githubaiscore/service/Mytest2.java
+++ src/main/java/run/mone/github/githubaiscore/service/Mytest2.java
@@ -8,7 +8,7 @@ public class Mytest2 {
 
         // 2. 常量与变量
         final int MAX = 100; // 常量
-        double price = 25.5; // 变量
+        double price = 100; // 变量
         System.out.println("\n常量MAX的值：" + MAX + "，变量price的值：" + price);
     }
 }
